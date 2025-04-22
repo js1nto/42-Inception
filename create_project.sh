e0d1c4afff8ee37c93e4e13ed26d66c9461d5dcf
Jules Sainthorant <jsaintho@paul-f4Ar4s4.clusters.42paris.fr>
2025-04-22 11:07:07 +0200

    clear scripts

--- create_project.sh
+++ create_project.sh
@@ -9,49 +9,36 @@ ADM_WP_PASS="1234567"
 
 echo "secrets" > project/.gitignore
 
-touch project/secrets/credentials.txt
-echo "LOGIN=$LOGIN" > project/secrets/credentials.txt
-echo "DOMAIN_NAME=$LOGIN.42.fr" > project/secrets/credentials.txt
-echo "CERT_=./requirements/tools/$LOGIN.42.fr.crt" >> project/secrets/credentials.txt
-echo "KEY_=./requirements/tools/$LOGIN.42.fr.key" >> project/secrets/credentials.txt
-echo "DB_NAME=wordpress" >> project/secrets/credentials.txt
-echo "DB_USER=wpuser" >> project/secrets/credentials.txt
-echo "DB_HOST=mariadb" >> project/secrets/credentials.txt
-echo "WP_TITLE=INCEPTION_$NAME" >> project/secrets/credentials.txt
-echo "WP_USERNAME=$NAME" >> project/secrets/credentials.txt
-echo "WP_USEREMAIL=$NAME@42.fr" >> project/secrets/credentials.txt
-echo "WP_USERPASS=$NAME" >> project/secrets/credentials.txt
-echo "WP_HOST=$LOGIN.42.fr" >> project/secrets/credentials.txt
-echo "ADM_WP_NAME=$NAME" >> project/secrets/credentials.txt
-echo "ADM_WP_EMAIL=$NAME@42.fr" >> project/secrets/credentials.txt
+if [ ! -f ./srcs/.env ]; then
+  touch ./srcs/.env
 
-echo $DB_PW > project/secrets/db_password.txt
-echo $DB_ROOT_PW > project/secrets/db_root_password.txt
-echo $WP_USERPASS > project/secrets/wp_password.txt
-echo $ADM_WP_PASS > project/secrets/adm_wp_password.txt
+# --- MAKE ENV ---------------------------
+echo "LOGIN=$LOGIN" >> ./srcs/.env
+echo "DOMAIN_NAME=$LOGIN.42.fr" >> ./srcs/.env
+echo "CERT_=./requirements/tools/$LOGIN.42.fr.crt" >> ./srcs/.env
+echo "KEY_=./requirements/tools/$LOGIN.42.fr.key" >> ./srcs/.env
+echo "DB_NAME=wordpress" >> ./srcs/.env
+echo "DB_USER=wpuser" >> ./srcs/.env
+echo "DB_HOST=mariadb" >> ./srcs/.env
+echo "WP_TITLE=INCEPTION_$NAME" >> ./srcs/.env
+echo "WP_USERNAME=$NAME" >> ./srcs/.env
+echo "WP_USEREMAIL=$NAME@42.fr" >> ./srcs/.env
+echo "WP_USERPASS=$NAME" >> ./srcs/.env
+echo "WP_HOST=$LOGIN.42.fr" >> ./srcs/.env
+echo "ADM_WP_NAME=$NAME" >> ./srcs/.env
+echo "ADM_WP_EMAIL=$NAME@42.fr" >> ./srcs/.env
 
-echo '#!/bin/bash
+echo "DB_ROOT=$DB_ROOT_PW" >> srcs/.env
+echo "WP_USERPASS=$WP_USERPASS" >> srcs/.env 
+echo "DB_PASS=$DB_PW" >> srcs/.env
+echo "ADM_WP_PASS=$ADM_WP_PASS" >> srcs/.env
+# ------------------------------------------------
 
-cp secrets/credentials.txt srcs/.env
 
-DB_PASSWORD_FILE=$(cat "secrets/db_root_password.txt")
-echo "DB_ROOT=$DB_PASSWORD_FILE" >> srcs/.env
 
-DB_PASSWORD_FILE=$(cat "secrets/wp_password.txt")
-echo "WP_USERPASS=$DB_PASSWORD_FILE" >> srcs/.env 
-
-DB_PASSWORD_FILE=$(cat "secrets/db_password.txt")
-echo "DB_PASS=$DB_PASSWORD_FILE" >> srcs/.env
-
-DB_PASSWORD_FILE=$(cat "secrets/adm_wp_password.txt")
-echo "ADM_WP_PASS=$DB_PASSWORD_FILE" >> srcs/.env' > project/make_env.sh
-
-chmod +x project/make_env.sh
-
-echo '#!/bin/bash
+# --- SSL CERTIFICATE ---------------------------
 
 mkcert -key-file srcs/requirements/nginx/tools/${USER}.42.fr.key -cert-file srcs/requirements/nginx/tools/${USER}.42.fr.crt https://${USER}.42.fr
 
-chmod 777 srcs/requirements/nginx/tools/${USER}.42.fr.key srcs/requirements/nginx/tools/${USER}.42.fr.crt' > project/make_certs.sh
-
-chmod +x project/make_certs.sh
+chmod 777 srcs/requirements/nginx/tools/${USER}.42.fr.key srcs/requirements/nginx/tools/${USER}.42.fr.crt
+# ------------------------------------------------
--- srcs/make_certs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkcert -key-file srcs/requirements/nginx/tools/${USER}.42.fr.key -cert-file srcs/requirements/nginx/tools/${USER}.42.fr.crt https://${USER}.42.fr
-
-chmod 777 srcs/requirements/nginx/tools/${USER}.42.fr.key srcs/requirements/nginx/tools/${USER}.42.fr.crt
