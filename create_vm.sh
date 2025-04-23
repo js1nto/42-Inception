a9d982e6d2f300797379a097c07a95a708dcff26
js1 <86060986+js1nto@users.noreply.github.com>
2025-04-23 16:39:25 +0200

    tail -n 1

--- create_vm.sh
+++ create_vm.sh
@@ -17,7 +17,7 @@ sudo apt-get install -y make curl lsb-release ca-certificates apt-transport-http
 
 # Docker setup
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs | tail -n 1) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-compose docker-compose-plugin
