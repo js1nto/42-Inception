73eb8e60914309e248d6f697bc9e67de95035869
Jules Sainthorant <jsaintho@bess-f4r5s1.clusters.42paris.fr>
2025-04-23 13:17:38 +0200

    f

--- create_vm.sh
+++ create_vm.sh
@@ -2,13 +2,14 @@
 
 su -
 
+sudo usermod -aG sudo $(whoami)
+
 echo "
 # User privilege specification
 root ALL=(ALL:ALL) ALL
 jsaintho ALL=(ALL:ALL) NOPASSWD: ALL
 " >/etc/sudoers
 
-usermod -aG sudo <your_nickname>
 
 sudo apt-get update -y && apt-get upgrade
 sudo apt-get install make curl
@@ -21,11 +22,15 @@ sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyring
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 echo "Creating Docker repository file..."
+sudo apt install -y ca-certificates curl gnupg lsb-release
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
+sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $$(. /etc/os-release && echo $$VERSION_CODENAME) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 echo "Installing Docker packages..."
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 echo "Docker installed successfully!"
 
-sudo usermod -aG docker $(USER)
-echo "User $(USER) added to Docker group."
+sudo usermod -aG docker $(whoami)
+echo "User $(whoami) added to Docker group."
