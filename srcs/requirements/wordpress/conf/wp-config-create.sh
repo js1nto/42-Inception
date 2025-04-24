b79e0046d616546f12f180cbd063ea65d78cc36a
js1 <86060986+js1nto@users.noreply.github.com>
2025-04-24 14:56:58 +0200

    Update wp-config-create.sh

--- srcs/requirements/wordpress/conf/wp-config-create.sh
+++ srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -5,6 +5,6 @@ if [ ! -f "/var/www/wp-config.php" ]; then
   /usr/local/bin/wp config create --dbname="${DB_NAME}" --dbuser="${DB_USER}" --dbpass="${DB_PASS}" --dbhost="${DB_HOST}" --force
   /usr/local/bin/wp config set FS_METHOD 'direct'
   
-  /usr/local/bin/wp core install --url="https://${WP_HOST}:10443" --title="${WP_TITLE}" --admin_user="${ADM_WP_NAME}" --admin_password="${ADM_WP_PASS}" --admin_email="${ADM_WP_EMAIL}"
+  /usr/local/bin/wp core install --url="https://${WP_HOST}" --title="${WP_TITLE}" --admin_user="${ADM_WP_NAME}" --admin_password="${ADM_WP_PASS}" --admin_email="${ADM_WP_EMAIL}"
   /usr/local/bin/wp user create "${WP_USERNAME}" "${WP_USEREMAIL}" --role="editor" --user_pass="${WP_USERPASS}"
 fi
