6ee1730c064c2416ac2920c723232f3ac84677cb
js1 <86060986+js1nto@users.noreply.github.com>
2025-04-25 12:14:18 +0200

    Update create_vm.sh

--- create_vm.sh
+++ create_vm.sh
@@ -18,7 +18,7 @@ sudo apt-get install -y make curl lsb-release ca-certificates apt-transport-http
 # Docker setup for Debian
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian bookworm stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
