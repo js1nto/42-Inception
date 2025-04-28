b4afc4e8c655c8ad34a31498b099ce21435a0acd
js1 <86060986+js1nto@users.noreply.github.com>
2025-04-28 11:51:05 +0200

    Update create_vm.sh

--- create_vm.sh
+++ create_vm.sh
@@ -25,6 +25,7 @@ echo "Creating Docker repository file..."
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo $VERSION_CODENAME) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 echo "Installing Docker packages..."
+su -
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 echo "Docker installed successfully!"
 
