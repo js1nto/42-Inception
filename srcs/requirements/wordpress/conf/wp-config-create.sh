b2b1a0f9b994822cc1bd1e72bfe6c8ec44034362
js1 <86060986+js1nto@users.noreply.github.com>
2025-04-30 18:31:23 +0200

    Update wp-config-create.sh

--- srcs/requirements/wordpress/conf/wp-config-create.sh
+++ srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -1,10 +1,34 @@
 #!/bin/sh
-cd /var/www/ 
+cd /var/www/
+
 if [ ! -f "/var/www/wp-config.php" ]; then
-  # wp cli update
-  /usr/local/bin/wp config create --dbname="${DB_NAME}" --dbuser="${DB_USER}" --dbpass="${DB_PASS}" --dbhost="${DB_HOST}" --force
+  # Create wp-config.php
+  /usr/local/bin/wp config create \
+    --dbname="${DB_NAME}" \
+    --dbuser="${DB_USER}" \
+    --dbpass="${DB_PASS}" \
+    --dbhost="${DB_HOST}" \
+    --force
+
+  # Set direct file system method
   /usr/local/bin/wp config set FS_METHOD 'direct'
-  
-  /usr/local/bin/wp core install --url="https://${WP_HOST}" --title="${WP_TITLE}" --admin_user="${ADM_WP_NAME}" --admin_password="${ADM_WP_PASS}" --admin_email="${ADM_WP_EMAIL}"
-  /usr/local/bin/wp user create "${WP_USERNAME}" "${WP_USEREMAIL}" --role="editor" --user_pass="${WP_USERPASS}"
+
+  # Install WordPress
+  /usr/local/bin/wp core install \
+    --url="https://${WP_HOST}" \
+    --title="${WP_TITLE}" \
+    --admin_user="${ADM_WP_NAME}" \
+    --admin_password="${ADM_WP_PASS}" \
+    --admin_email="${ADM_WP_EMAIL}"
+
+  # Create an editor-level user
+  /usr/local/bin/wp user create "${WP_USERNAME}" "${WP_USEREMAIL}" \
+    --role="editor" \
+    --user_pass="${WP_USERPASS}"
+
+  # ✅ Create a read-only (subscriber) user
+  /usr/local/bin/wp user create "${WP_READONLY_NAME}" "${WP_READONLY_EMAIL}" \
+    --role="subscriber" \
+    --user_pass="${WP_READONLY_PASS}"
 fi
+
