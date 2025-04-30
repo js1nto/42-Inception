2d8e5b9c5f47773ca1a2bd1a236a34a8a074be18
js1 <86060986+js1nto@users.noreply.github.com>
2025-04-30 12:25:58 +0200

    Update create_db.sh

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -1,39 +1,47 @@
 #!/bin/bash
 
-#if [ -z "$DB_PASS" ] || [ -z "$DB_ROOT" ] || [ -z "$DB_NAME" ]; then
-
-#  echo "Erreur : Toutes les variables d'environnement (DB_PASS, DB_ROOT, DB_NAME) doivent être définies."
-#  exit 1
-#fi
+# Optional: Check required environment variables
+# Uncomment if needed
+# if [ -z "$DB_PASS" ] || [ -z "$DB_ROOT" ] || [ -z "$DB_NAME" ] || [ -z "$DB_USER" ]; then
+#     echo "Erreur : Toutes les variables d'environnement (DB_PASS, DB_ROOT, DB_NAME, DB_USER) doivent être définies."
+#     exit 1
+# fi
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
+    chown -R mysql:mysql /var/lib/mysql
 
-        chown -R mysql:mysql /var/lib/mysql
+    # Initialize the MariaDB system database
+    mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
 
-        # init database
-        mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
-
-        tfile=`mktemp`
-        if [ ! -f "$tfile" ]; then
-                return 1
-        fi
+    tfile=$(mktemp)
+    if [ ! -f "$tfile" ]; then
+        echo "Erreur : impossible de créer un fichier temporaire."
+        exit 1
+    fi
 fi
 
 if [ ! -d "/var/lib/mysql/wordpress" ]; then
 
-        cat << EOF > /tmp/create_db.sql
+    cat << EOF > /tmp/create_db.sql
 USE mysql;
 FLUSH PRIVILEGES;
-DELETE FROM     mysql.user WHERE User='';
-DELETE FROM     mysql.user WHERE User='wordpress_user';
-DROP DATABASE test;
+
+-- Clean up default users and test DB
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='wordpress_user';
+DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT}';
+
+-- Set root password (MariaDB-compatible way)
+UPDATE mysql.user SET Password = PASSWORD('${DB_ROOT}') WHERE User = 'root' AND Host = 'localhost';
+
+-- Create WordPress DB and user
 CREATE DATABASE ${DB_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER '${DB_USER}'@'%' IDENTIFIED by '${DB_PASS}';
-GRANT ALL PRIVILEGES ON wordpress.* TO '${DB_USER}'@'%';
+CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
 FLUSH PRIVILEGES;
+
 -- Create non-admin WordPress user (subscriber)
 USE ${DB_NAME};
 
@@ -42,11 +50,11 @@ SET @username = 'user_';
 SET @password = MD5('1234'); -- Replace with a secure password
 SET @email = 'comment_user@example.com';
 
--- Insert into wp_users
+-- Insert user into wp_users
 INSERT INTO wp_users (user_login, user_pass, user_nicename, user_email, user_status, display_name)
 VALUES (@username, @password, @username, @email, 0, @username);
 
--- Get the new user ID
+-- Get new user ID
 SET @user_id = LAST_INSERT_ID();
 
 -- Assign subscriber role
@@ -56,9 +64,9 @@ VALUES
 (@user_id, 'wp_user_level', '0');
 EOF
 
-    # Run the SQL script using MySQL bootstrap mode
+    # Execute the SQL script in bootstrap mode
     /usr/bin/mysqld --user=mysql --bootstrap < /tmp/create_db.sql
 
-    # Optional: clean up
+    # Optional: Clean up
     # rm -f /tmp/create_db.sql
 fi
