d6f4821b2cd65fd75cbdbc4ab70c27e8c9843cd0
js1 <86060986+js1nto@users.noreply.github.com>
2025-05-02 12:30:08 +0200

    Update wp-config-create.sh

--- srcs/requirements/wordpress/conf/wp-config-create.sh
+++ srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -1,32 +1,10 @@
 #!/bin/sh
-cd /var/www/
-
+cd /var/www/ 
 if [ ! -f "/var/www/wp-config.php" ]; then
-  /usr/local/bin/wp config create \
-    --dbname="${DB_NAME}" \
-    --dbuser="${DB_USER}" \
-    --dbpass="${DB_PASS}" \
-    --dbhost="${DB_HOST}" \
-    --force
-
+  # wp cli update
+  /usr/local/bin/wp config create --dbname="${DB_NAME}" --dbuser="${DB_USER}" --dbpass="${DB_PASS}" --dbhost="${DB_HOST}" --force
   /usr/local/bin/wp config set FS_METHOD 'direct'
-
-  /usr/local/bin/wp core install \
-    --url="https://${WP_HOST}" \
-    --title="${WP_TITLE}" \
-    --admin_user="${ADM_WP_NAME}" \
-    --admin_password="${ADM_WP_PASS}" \
-    --admin_email="${ADM_WP_EMAIL}"
+  
+  /usr/local/bin/wp core install --url="https://${WP_HOST}" --title="${WP_TITLE}" --admin_user="${ADM_WP_NAME}" --admin_password="${ADM_WP_PASS}" --admin_email="${ADM_WP_EMAIL}"
+  /usr/local/bin/wp user create "${WP_USERNAME}" "${WP_USEREMAIL}" --role="editor" --user_pass="${WP_USERPASS}"
 fi
-
-# Always attempt to create/update users
-#/usr/local/bin/wp user get "${WP_USERNAME}" >/dev/null 2>&1 || \
-#/usr/local/bin/wp user create "${WP_USERNAME}" "${WP_USEREMAIL}" \
-#  --role="editor" \
-#  --user_pass="${WP_USERPASS}"
-
-#/usr/local/bin/wp user get "${WP_READONLY_NAME}" >/dev/null 2>&1 || \
-#/usr/local/bin/wp user create "${WP_READONLY_NAME}" "${WP_READONLY_EMAIL}" \
-#  --role="subscriber" \
-#  --user_pass="${WP_READONLY_PASS}"
-
