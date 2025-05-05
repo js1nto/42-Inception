f6bc92156676ceb5b3f41a004a704d2e415cb971
youradrien3003 <86060986+js1nto@users.noreply.github.com>
2025-05-05 11:53:30 +0200

    Update create_db.sh

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -8,23 +8,23 @@ mysqld_safe --datadir=/var/lib/mysql &
 
 sleep 5
 
-if [ -z "$DB_USER" ] || [ -z "$DB_USERPASS" ] || [ -z "$DB_USERPASS" ] || [ -z "$DB_NAME" ]; then
-  echo "Erreur : Toutes les variables d'environnement (DB_USER, DB_USERPASS, DB_NAME, MARIADB_NAME) doivent être définies."
+if [ -z "$MARIADB_USER" ] || [ -z "$MARIADB_USER_PASSWORD" ] || [ -z "$MARIADB_ROOT_PASSWORD" ] || [ -z "$MARIADB_NAME" ]; then
+  echo "Erreur : Toutes les variables d'environnement (MARIADB_USER, MARIADB_USER_PASSWORD, MARIADB_ROOT_PASSWORD, MARIADB_NAME) doivent être définies."
   exit 1
 fi
 
 echo "FLUSH PRIVILEGES;" | mysql -u root
 
-echo "DROP USER IF EXISTS '$DB_USER'@'%';" | mysql -u root
+echo "DROP USER IF EXISTS '$MARIADB_USER'@'%';" | mysql -u root
 
-echo "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_USERPASS';" | mysql -u root
+echo "CREATE USER '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_USER_PASSWORD';" | mysql -u root
 
-echo "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_USERPASS';" | mysql -u root
-echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$DB_ROOT';" | mysql -u root
+echo "GRANT ALL PRIVILEGES ON *.* TO '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_USER_PASSWORD';" | mysql -u root
+echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD';" | mysql -u root
 
-echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$DB_ROOT';" | mysql -u root
+echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD';" | mysql -u root
 
-echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" | mysql -u root
+echo "CREATE DATABASE IF NOT EXISTS $MARIADB_NAME;" | mysql -u root
 
 echo "FLUSH PRIVILEGES;" | mysql -u root
 
